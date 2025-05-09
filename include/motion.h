4818e9da99a1c1e83afabaf11d6133d1fb9a9c6d
harley <480497125@qq.com>
2025-05-09 23:10:01 +0800

    修正为速度模式加pid

--- include/motion.h
+++ include/motion.h
@@ -9,28 +9,12 @@ struct POS{
     double y;
     float yaw;
 };
-
-enum systemState
-{
-  IDLE,               // 空闲状态
-  COARSE_POSITIONING, // 粗定位状态
-  FINE_POSITIONING,   // 精定位状态
-  COMPLETED           // 定位完成
-};
-
 // 外部声明电机驱动实例，使其全局可用
 extern ZDTX42V2 *motor;
 extern systemState currentState;
-float calculateMinAngleDifference(float angle1, float angle2);
 void initMotor(void);
-void moveTo(POS _targetPos);
-void stopMotion(void);
-void emergencyStopMotor(bool is_enable);
-void globalToLocalVelocity(float global_vx, float global_vy, float yaw_rad, float &local_vx, float &local_vy);
-void calculateWheelVelocities(float vx, float vy, float omega, float wheelVelocities[4]);
-void calculateWheelPositions(float wheelPositions[4]);
-float calculateDistance(float x1, float y1, float x2, float y2);
+void moveTo(POS pos, float speed=DEFAULT_SPEED, int acc=DEFAULT_ACC, int dec=DEFAULT_ACC);
 void moveTask(void*pvParameters);
-bool coarsePositioning(void);
-bool finePositioning(void);
-void updateCurrentPosition();
+bool arrived(void);//返回true表示到达目标点
+void stopMotion(void);//停止运动并释放电机控制
+void forceStopAllMotors(void);//强制停止所有电机，确保速度为0
